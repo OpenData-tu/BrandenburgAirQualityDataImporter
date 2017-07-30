39f55d9dcdec9a1bfc18a5fb7586448e416de30f
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-07-30 22:42:13 +0200

    Minor commenting fix

--- application/src/main/java/de/tu_berlin/ise/open_data/airquality/batch/BatchConfiguration.java
+++ application/src/main/java/de/tu_berlin/ise/open_data/airquality/batch/BatchConfiguration.java
@@ -135,7 +135,7 @@ public class BatchConfiguration {
 
     /**
      * Registers a job step named 'step1' which defines how to read, process and write
-     * @return {@link Job}
+     * @return {@link Step}
      * */
     @Bean
     Step step1() throws IOException, InvalidFormatException {
